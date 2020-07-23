1843d9bac5448102c7181ab0b6f361aa91787634
Paul J Shaw <pavlvsxavier@gmail.com>
2020-07-23 03:31:25 +0100

    added page to show error connecting to db

--- model/database.php
+++ model/database.php
@@ -1,9 +1,19 @@
 <?php
+$dsn = 'mysql:host=localhost;dbname=my_guitar_shop2';
+$username = 'mgs_user';
+$password = 'pa55word';
+$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
 try {
-    $db = new PDO('mysql:host=localhost;dbname=my_guitar_shop1;charset=utf8', 'mgs_user', 'pa55word');
-    // echo '<p>You are connected to the database';
+    $db = new PDO($dsn, $username, $password, $options);
 } catch (PDOException $e) {
-    $errorMessage = $e->getMessage();
-    include 'database_error.php';
-    exit();
+    $error_message = $e->getMessage();
+    include 'errors/db_error_connect.php';
+    exit;
+}
+
+function display_db_error($error_message) {
+    global $app_path;
+    include 'errors/db_error.php';
+    exit;
 }
