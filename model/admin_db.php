27619ca14a53457be0a60401ac87ff31e7612967
Paul J Shaw <pavlvsxavier@gmail.com>
2020-07-25 08:22:01 +0100

    wrote admin model and common functions in main.php

--- model/admin_db.php
+++ model/admin_db.php
@@ -1,14 +1,18 @@
 <?php
-function addAdmin($email, $password) {
+function addAdmin($email, $firstName, $lastName, $password) {
     global $db;
     $password = sha1($email . $password);
-    $sql = 'INSERT INTO administrators (emailAddress, password)
-            VALUES (:email, :password)';
+    $sql = 'INSERT INTO administrators (emailAddress, password, firstName, lastName)
+            VALUES (:email, :password, :firstName, :lastName)';
     $stmt = $db->prepare($sql);
     $stmt->bindValue(':email', $email);
     $stmt->bindValue(':password', $password);
+    $stmt->bindValue(':firstName', $firstName);
+    $stmt->bindValue(':lastName', $lastName);
     $stmt->execute();
+    $adminId = $db->lastInsertId();
     $stmt->closeCursor();
+    return $adminId;
 }
 
 function isValidAdminLogin($email, $password) {
@@ -24,4 +28,99 @@ function isValidAdminLogin($email, $password) {
     $valid = ($stmt->rowCount() == 1);
     $stmt->closeCursor();
     return $valid;
+}
+
+function admincount() {
+    global $db;
+    $sql = 'SELECT COUNT(*) AS adminCount FROM administrators';
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetch();
+    $stmt->closeCursor();
+    return $result['adminCount'];
+}
+
+function getAllAdmins() {
+    global $db;
+    $sql = 'SELECT * FROM administrators ORDER BY lastname, firstName';
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $admins = $stmt->fetchAll();
+    $stmt->closeCursor();
+    return $admins;
+}
+
+function getAdmin($adminId) {
+    global $db;
+    $sql = 'SELECT * FROM administrators WHERE adminID = :adminId';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':adminId', $adminId);
+    $stmt->execute();
+    $admin = $stmt->fetch();
+    $stmt->closeCursor();
+    return $admin;
+}
+
+function getAdminByEmail($email) {
+    global $db;
+    $sql = 'SELECT * FROM administrators WHERE emailAddress = :email';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':email', $email);
+    $stmt->execute();
+    $admin = $stmt->fetch();
+    $stmt->closeCursor();
+    return $admin;
+}
+
+function isValidAdminEmail($email) {
+    global $db;
+    $sql = 'SELECT * FROM administrators WHERE emailAddress = :email';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':email', $email);
+    $stmt->execute();
+    $valid = ($stmt->rowCount() == 1);
+    $stmt->closeCursor();
+    return $valid;
+}
+
+function updateAdmin($adminId, $email, $firstName, $lastName, $password1, $password2) {
+    global $db;
+    $sql = 'UPDATE administrators
+            SET emailAddress = :email,
+                firstName    = :firstName,
+                lastName     = :lastName,
+            WHERE adminID    = :adminId';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':email', $email);
+    $stmt->bindValue(':firstName', $firstName);
+    $stmt->bindValue(':lastName', $lastName);
+    $stmt->bindValue(':adminId', $adminId);
+    $stmt->execute();
+    $stmt->closeCursor();
+
+    if (!empty($password1) && !empty($password2)) {
+        if ($password1 !== $password2) {
+            displayError('passwords do not match.button_form');
+        } elseif (strlen($password1 < 6)) {
+            displayError('Password must be at least six characters');
+        }
+        $password = sha1($email . $password1);
+        $sql = 'UPDATE administrators
+                SET password = :password
+                WHERE adminID = :adminId';
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':password', $password);
+        $stmt->bindValue(':adminId', $adminId);
+        $stmt->execute();
+        $stmt->closeCursor();
+    }
+}
+
+function deleteAdmin($adminId) {
+    global $db;
+    $sql = 'DELETE FROM administrators WHERE adminID = :adminId';
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':adminid', $adminId);
+    $stmt->execute();
+    $stmt->closeCursor();
 }
--- util/main.php
+++ util/main.php
@@ -10,3 +10,29 @@ $appPath = '/';
 
 // Set the include path
 set_include_path($docRoot . $appPath);
+
+// Get common code
+require_once 'util/tags.php';
+require_once 'model/database.php';
+
+// Define some common functions
+function displayDBError($errorMessage) {
+    global $appPath;
+    include 'errors/db_error.php';
+    exit;
+}
+
+function displayError($errorMessage) {
+    global $appPath;
+    include 'errors/error.php';
+    exit;
+}
+
+function redirect($url) {
+    session_write_close();
+    header('Location: ' . $url);
+    exit;
+}
+
+// start session to store user and cart data
+session_start();
